6d1aa51ab3236af9fab6b0793f1beeff9b189378
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-28 01:45:28 +0900

    [chore] make project

---
+++ OneTone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneTone
+//
+//  Created by 村石 拓海 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
